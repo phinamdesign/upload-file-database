7d14484eac4a328a3d18a3212aa1a3507887920a
Phi Nam <phinamhd1992@gmail.com>
2020-01-13 08:08:37 +0700

    upload-file-database-api

---
+++ src/main/java/net/javaguides/springboot/fileuploaddownload/SpringbootUploadDownloadFileRestApiExampleApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot.fileuploaddownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootUploadDownloadFileRestApiExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootUploadDownloadFileRestApiExampleApplication.class, args);
+    }
+
+}
